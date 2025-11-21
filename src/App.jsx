9ab3a0b67d0bacbb9c68c07b5ce93f5c288899b4
Prakhar Shukla <prakharshukla297@gmail.com>
2025-11-21 12:13:51 +0530

    Day 1 learning update

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Name />
+      <Age />
+    </div>
+  );
+}
+
+function Name() {
+  return (
+    <div>
+      <h2>Prakhar Shukla</h2>
+    </div>
+  );
+}
+
+function Age() {
+  return (
+    <div>
+      <h2>22</h2>
+    </div>
+  );
+}
+
+export default App;
